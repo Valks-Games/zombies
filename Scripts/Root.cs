af9c749c37174d007a653bbe9033fe24ac3d3039
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-11-30 04:02:56 -0600

    First commit

---
+++ Scripts/Root.cs
@@ -0,0 +1,25 @@
+global using Godot;
+global using System;
+global using System.Collections.Generic;
+global using System.Collections.Concurrent;
+global using System.Diagnostics;
+global using System.Runtime.CompilerServices;
+global using System.Threading;
+global using System.Text.RegularExpressions;
+global using System.Threading.Tasks;
+global using System.Linq;
+
+namespace Zombies;
+
+public partial class Root : Node
+{
+	public override void _Ready()
+	{
+
+	}
+
+	public override void _Process(double delta)
+	{
+
+	}
+}
---
+++ Scripts/Utils/Extensions/MathExtensions.cs
@@ -0,0 +1,6 @@
+﻿namespace Zombies;
+
+public static class MathExtensions
+{
+	public static float ToRadians(this float degrees) => degrees * (Mathf.Pi / 180);
+}
