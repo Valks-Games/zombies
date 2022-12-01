f80469670f65e43da6e5378597bc20777b5a331c
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-12-01 00:16:20 -0600

    first attempt at weapon recoil

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -7,26 +7,38 @@ namespace Zombies;
 
 public partial class Player : CharacterBody3D
 {
-	[Export] public float MouseSensitivity { get; set; } = 0.5f;
-	[Export] public float GravityForce { get; set; } = 10;
-	[Export] public float JumpForce { get; set; } = 150;
-	[Export] public float MoveSpeed { get; set; } = 10;
-	[Export] public float MoveDampening { get; set; } = 20; // the higher the value, the less the player will slide
+	[Export] public float   MouseSensitivity         { get; set; } = 0.5f;
+	[Export] public float   GravityForce             { get; set; } = 10;
+	[Export] public float   JumpForce                { get; set; } = 150;
+	[Export] public float   MoveSpeed                { get; set; } = 10;
+	[Export] public float   MoveDampening            { get; set; } = 20; // the higher the value, the less the player will slide
+											   
+	private Camera3D        Camera                   { get; set; }
+	private Camera3D        GunCam                   { get; set; }
+	private RayCast3D       RayCast                  { get; set; }
+	private Node3D          Gun                      { get; set; }
+	private AnimationPlayer GunAnim                  { get; set; }
+	private Vector3         GravityVec               { get; set; }
 
-	private Camera3D Camera { get; set; }
-	private Camera3D GunCam { get; set; }
-	private RayCast3D RayCast { get; set; }
-	private Node3D Gun { get; set; }
-	private AnimationPlayer GunAnim { get; set; }
-	private Vector3 GravityVec { get; set; }
+	private float           StartingRecoilHorz       { get; set; } = 0.01f;
+	private float           RecoilGainHorz           { get; set; } = 0.0005f;
+	private float           MaxRecoilHorz            { get; set; } = 0.05f;
+	private float           RecoilHorz               { get; set; }
+					          				       
+	private float           StartingRecoilVert       { get; set; } = 0.001f;
+	private float           RecoilGainVert           { get; set; } = 0.0001f; 
+	private float           MaxRecoilVert            { get; set; } = 0.005f;
+	private float           RecoilVert               { get; set; }
 
 	public override void _Ready()
 	{
-		Camera = GetNode<Camera3D>("Camera3D");
-		GunCam = GetNode<Camera3D>("Camera3D/SubViewportContainer/SubViewport/Camera3D");
-		Gun = GetNode<Node3D>("Camera3D/Gun");
+		RecoilHorz = StartingRecoilHorz;
+
+		Camera  = GetNode<Camera3D>       ("Camera3D");
+		GunCam  = GetNode<Camera3D>       ("Camera3D/SubViewportContainer/SubViewport/Camera3D");
+		Gun     = GetNode<Node3D>         ("Camera3D/Gun");
 		GunAnim = GetNode<AnimationPlayer>("Camera3D/Gun/AnimationPlayer");
-		RayCast = GetNode<RayCast3D>("Camera3D/RayCast");
+		RayCast = GetNode<RayCast3D>      ("Camera3D/RayCast");
 
 		Input.MouseMode = MouseMode.Captured;
 	}
@@ -47,6 +59,7 @@ public partial class Player : CharacterBody3D
 		{
 			if (RayCast.IsColliding())
 			{
+				// create a temporary sphere at the raycast collision point
 				var sphere = new MeshInstance3D();
 
 				sphere.Mesh = new SphereMesh();
@@ -65,7 +78,28 @@ public partial class Player : CharacterBody3D
 				GetTree().Root.AddChild(sphere);
 			}
 
+			// recoil
+			RecoilHorz += RecoilGainHorz;
+			RecoilHorz = Mathf.Clamp(RecoilHorz, StartingRecoilHorz, MaxRecoilHorz);
+
+			RecoilVert += RecoilGainVert;
+			RecoilVert = Mathf.Clamp(RecoilVert, StartingRecoilVert, MaxRecoilVert);
+
+			var recoilVec = new Vector3(RecoilVert, (new Random().NextSingle() - 0.5f) * RecoilHorz, 0);
+
+			RayCast.Rotation += recoilVec;
+			Camera.Rotation += new Vector3(recoilVec.x, 0, 0);
+
+			// play gun animation
 			GunAnim.Play("fire");
+		}
+		else
+		{
+			// reset recoil instantly
+			RecoilHorz = StartingRecoilHorz;
+			RecoilVert = StartingRecoilVert;
+			RayCast.Rotation = Vector3.Zero;
+			//Camera.Rotation = Camera.Rotation.Lerp(Vector3.Zero, 0.1f);
 		}
 
 		var h_rot = GlobalTransform.basis.GetEuler().y;
