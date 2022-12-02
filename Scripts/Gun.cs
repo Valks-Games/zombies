7a8bfc7241c5fc26aec4255c871af9132e7254ef
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-12-02 13:43:38 -0600

    Separate gun code from player code

---
+++ Scripts/Gun.cs
@@ -0,0 +1,45 @@
+namespace Zombies;
+
+public partial class Gun : Node
+{
+	[Export] public Vector2 Spray { get; set; } // the guns spray offset
+	[Export] public Vector3 Recoil { get; set; }
+	[Export] public float ReturnSpeed { get; set; } = 1;
+
+	private Player Player { get; set; }
+	private AnimationPlayer AnimationPlayer { get; set; }
+
+	public void Init(Player player)
+	{
+		Player = player;
+	}
+
+	public override void _Ready()
+	{
+		AnimationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+	}
+
+	public void Shoot(float delta)
+	{
+		Player.RayCast.Rotation = Vector3.Zero;
+		Player.RayCast.RotateX(Math.RandomRange(-Spray.x * delta, Spray.x * delta));
+		Player.RayCast.RotateY(Math.RandomRange(-Spray.y * delta, Spray.y * delta));
+
+		var recoilY = Math.RandomRange(-Recoil.y * delta, Recoil.y * delta);
+		var recoilZ = Math.RandomRange(-Recoil.z * delta, Recoil.z * delta);
+
+		var recoilVec = new Vector3(Recoil.x * delta, recoilY, recoilZ);
+
+		Player.CameraOffset += recoilVec;
+		//CameraTarget += new Vector3(recoilVec.x / 2, 0, 0); // Only return halfway
+
+		if (Player.RayCast.IsColliding())
+		{
+			// create a temporary sphere at the raycast collision point
+			Geometry.CreateSphere(Player.RayCast.GetCollisionPoint(), 0.2f, 2);
+		}
+
+		// play gun animation
+		AnimationPlayer.Play("fire");
+	}
+}
--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -8,27 +8,23 @@ public partial class Player : CharacterBody3D
 	[Export] public float   MoveSpeed                { get; set; } = 10;
 	[Export] public float   MoveDampening            { get; set; } = 20; // the higher the value, the less the player will slide
 	
-	[Export] public Vector2 Spray { get; set; } // the guns spray offset
-	[Export] public Vector3 Recoil { get; set; }
-	[Export] public float ReturnSpeed { get; set; } = 1;
-	
 	private Camera3D        Camera                   { get; set; }
 	private Camera3D        GunCam                   { get; set; }
-	private RayCast3D       RayCast                  { get; set; }
-	private Node3D          Gun                      { get; set; }
-	private AnimationPlayer GunAnim                  { get; set; }
+	public  RayCast3D       RayCast                  { get; set; }
+	private Gun             Gun                      { get; set; }
 	private Vector3         GravityVec               { get; set; }
 
-	private Vector3 CameraTarget { get; set; }
-	private Vector3 CameraOffset { get; set; }
+	public Vector3 CameraTarget { get; set; }
+	public Vector3 CameraOffset { get; set; }
 
 	public override void _Ready()
 	{
 		Camera  = GetNode<Camera3D>       ("Camera3D");
 		GunCam  = GetNode<Camera3D>       ("Camera3D/SubViewportContainer/SubViewport/Camera3D");
-		Gun     = GetNode<Node3D>         ("Camera3D/Gun");
-		GunAnim = GetNode<AnimationPlayer>("Camera3D/Gun/AnimationPlayer");
 		RayCast = GetNode<RayCast3D>      ("Camera3D/RayCast");
+		Gun     = GetNode<Gun>            ("Camera3D/Gun");
+
+		Gun.Init(this);
 	}
 
 	public override void _Process(double delta)
@@ -40,32 +36,11 @@ public partial class Player : CharacterBody3D
 	{
 		var delta = (float)d;
 
-		CameraOffset = CameraOffset.Lerp(Vector3.Zero, delta * ReturnSpeed);
+		CameraOffset = CameraOffset.Lerp(Vector3.Zero, delta * Gun.ReturnSpeed);
 		Camera.Rotation = CameraTarget + CameraOffset;
 
 		if (Input.IsActionPressed("shoot"))
-		{
-			RayCast.Rotation = Vector3.Zero;
-			RayCast.RotateX(Math.RandomRange(-Spray.x * delta, Spray.x * delta));
-			RayCast.RotateY(Math.RandomRange(-Spray.y * delta, Spray.y * delta));
-
-			var recoilY = Math.RandomRange(-Recoil.y * delta, Recoil.y * delta);
-			var recoilZ = Math.RandomRange(-Recoil.z * delta, Recoil.z * delta);
-
-			var recoilVec = new Vector3(Recoil.x * delta, recoilY, recoilZ);
-
-			CameraOffset += recoilVec;
-			CameraTarget += new Vector3(recoilVec.x / 2, 0, 0);
-
-			if (RayCast.IsColliding())
-			{
-				// create a temporary sphere at the raycast collision point
-				Geometry.CreateSphere(RayCast.GetCollisionPoint(), 0.2f, 2);
-			}
-
-			// play gun animation
-			GunAnim.Play("fire");
-		}
+			Gun.Shoot(delta);
 
 		//var h_rot = GlobalTransform.basis.GetEuler().y;
 		var h_rot = Camera.Basis.GetEuler().y;
