61df3e7a53982854bf245805e5d687060990ae82
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-12-06 16:26:14 -0600

    Zombies respect each others space

--- Scripts/Zombie.cs
+++ Scripts/Zombie.cs
@@ -5,6 +5,7 @@ public partial class Zombie : CharacterBody3D
 	private AnimationPlayer AnimationPlayer { get; set; }
 	private AnimationTree AnimationTree { get; set; }
 	private NavigationAgent3D NavigationAgent3D { get; set; }
+	private Skeleton3D Skeleton3D { get; set; }
 
 	private Godot.Timer Timer { get; set; }
 	private bool ready = false;
@@ -14,9 +15,11 @@ public partial class Zombie : CharacterBody3D
 		//AnimationPlayer = GetNode<AnimationPlayer>("GLTF/AnimationPlayer");
 		//AnimationPlayer.Play("walk");
 
-		AnimationTree = GetNode<AnimationTree>("GLTF/AnimationTree");
+		AnimationTree = GetNode<AnimationTree>("AnimationTree");
 
 		NavigationAgent3D = GetNode<NavigationAgent3D>("NavigationAgent3D");
+
+		Skeleton3D = GetNode<Skeleton3D>("Armature/Skeleton3D");
 
 		Timer = new Godot.Timer();
 		Timer.WaitTime = 1;
@@ -44,7 +47,7 @@ public partial class Zombie : CharacterBody3D
 		var nextLocation = NavigationAgent3D.GetNextLocation();
 		var newVelocity = (nextLocation - Position).Normalized();
 
-		Geometry.CreateSphere(nextLocation, 0.2f, 2);
+		//Geometry.CreateSphere(nextLocation, 0.2f, 2);
 
 		var dir = nextLocation - Position;
 
@@ -57,16 +60,25 @@ public partial class Zombie : CharacterBody3D
 		if (Rotation.y < prevRot.y + 0.01f && Rotation.y > prevRot.y - 0.01f)
 		{
 			animationMotion = Mathf.Lerp(animationMotion, 0.5f, 0.02f);
-			Velocity = Velocity.MoveToward(newVelocity, 0.1f);
+			NavigationAgent3D.SetVelocity(newVelocity);
 		}
 		else
 		{ 
 			animationMotion = Mathf.Lerp(animationMotion, 0f, 0.02f);
-			Velocity = Vector3.Zero;
+			NavigationAgent3D.SetVelocity(Vector3.Zero);
 		}
 
 		prevRot = Rotation;
+	}
 
+	private void _on_navigation_agent_3d_velocity_computed(Vector3 safeVelocity)
+	{
+		Velocity = Velocity.MoveToward(safeVelocity, 0.1f);
 		MoveAndSlide();
+	}
+
+	private void _on_navigation_agent_3d_target_reached()
+	{
+
 	}
 }
