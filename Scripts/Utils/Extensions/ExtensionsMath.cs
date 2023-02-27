bbc7cb8da10d9d28e495e37e0a63ff64b4252503
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-02-26 19:44:22 -0600

    Add GodotUtils

--- Scripts/Utils/Extensions/ExtensionsMath.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Zombies;
-
-public static class ExtensionsMath
-{
-	public static float ToRadians(this float degrees) => degrees * (Mathf.Pi / 180);
-	public static float ToAngles(this float radians) => radians * (180 / Mathf.Pi); 
-}
